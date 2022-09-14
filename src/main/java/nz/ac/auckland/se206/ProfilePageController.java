092b2f528ea27a641c1cff9a517bb87d3c422d18
James <jgai284@aucklanduni.ac.nz>
2022-09-14 19:35:48 +1200

    profile page added with labels and buttons

---
+++ src/main/java/nz/ac/auckland/se206/ProfilePageController.java
@@ -0,0 +1,22 @@
+package nz.ac.auckland.se206;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class ProfilePageController {
+
+	@FXML private Button back;
+	@FXML private Label username;
+	@FXML private Label win;
+	@FXML private Label loss;
+	
+	@FXML
+	private void onBack (ActionEvent event) {
+		
+	}
+	
+	
+
+}
