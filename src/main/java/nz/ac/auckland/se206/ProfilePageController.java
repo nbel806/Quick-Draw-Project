6cdd8f480979e7e9a2bd728df2a7febd53c15872
James <jgai284@aucklanduni.ac.nz>
2022-09-16 00:38:37 +1200

    profile pages has been linked to the main menu

--- src/main/java/nz/ac/auckland/se206/ProfilePageController.java
+++ src/main/java/nz/ac/auckland/se206/ProfilePageController.java
@@ -1,9 +1,14 @@
 package nz.ac.auckland.se206;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class ProfilePageController {
 
@@ -13,8 +18,18 @@ public class ProfilePageController {
 	@FXML private Label loss;
 	
 	@FXML
-	private void onBack (ActionEvent event) {
+	private void onMainMenu () throws IOException {
 		
+		Stage stage = (Stage) back.getScene().getWindow();
+	    FXMLLoader loader =
+	        new FXMLLoader(
+	            App.class.getResource("/fxml/main_menu.fxml")); // creates a new instance of menu page
+	    Scene scene = new Scene(loader.load(), 1000, 680);
+	    MainMenuController ctrl = loader.getController(); // need controller to pass information
+	    //may need to add code to pass though tts here
+	    stage.setScene(scene);
+	    stage.show();
+	    
 	}
 	
 	
