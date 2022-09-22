4e63eb46deb7585d02de15684529eaca13a43437
James <jgai284@aucklanduni.ac.nz>
2022-09-22 22:33:39 +1200

    improvement made: added menu button in the end page that allows you to go straight to the menu

--- src/main/java/nz/ac/auckland/se206/GameOverController.java
+++ src/main/java/nz/ac/auckland/se206/GameOverController.java
@@ -16,146 +16,155 @@ import nz.ac.auckland.se206.speech.TextToSpeechBackground;
 import nz.ac.auckland.se206.words.WordPageController;
 
 public class GameOverController {
-  @FXML
-  private Label winLoseLabel2;
-  @FXML private Label winLoseLabel;
-  @FXML private Button playAgainButton;
-  @FXML private Label textToSpeechLabel;
+	@FXML
+	private Label winLoseLabel2;
+	@FXML
+	private Label winLoseLabel;
+	@FXML
+	private Button playAgainButton;
+	@FXML
+	private Button menuButton;
+	@FXML
+	private Label textToSpeechLabel;
 
-  private CanvasController canvasController;
-  private Boolean textToSpeech;
-  private TextToSpeechBackground textToSpeechBackground;
+	private CanvasController canvasController;
+	private Boolean textToSpeech;
+	private TextToSpeechBackground textToSpeechBackground;
 
-  private String winLoseString;
+	private String winLoseString;
 
-  private int timeLeft;
-  private String currentUsername;
+	private int timeLeft;
+	private String currentUsername;
 
+	public void give(TextToSpeechBackground textToSpeechBackground, Boolean textToSpeech) {
+		this.textToSpeechBackground = textToSpeechBackground;
+		this.textToSpeech = textToSpeech;
+		if (textToSpeech) { // updates label to ensure it is correct
+			textToSpeechLabel.setText("ON");
+		}
+	}
 
-  public void give(TextToSpeechBackground textToSpeechBackground, Boolean textToSpeech) {
-    this.textToSpeechBackground = textToSpeechBackground;
-    this.textToSpeech = textToSpeech;
-    if (textToSpeech) { // updates label to ensure it is correct
-      textToSpeechLabel.setText("ON");
-    }
-  }
+	public void setWinLoseLabel(boolean winLose, CanvasController ctrl) throws IOException, CsvException {
+		if (winLose) {
+			winLoseLabel.setText("You won with " + timeLeft);
+			winLoseLabel2.setText("seconds left!");
 
-  public void setWinLoseLabel(boolean winLose, CanvasController ctrl)
-      throws IOException, CsvException {
-    if(winLose){
-      winLoseLabel.setText("You won with " + timeLeft);
-      winLoseLabel2.setText("seconds left!");
+			winLoseString = "You won with " + timeLeft + "Seconds left!";
+		} else {
+			winLoseLabel.setText("You lost!");
+			winLoseLabel2.setText("");
+			winLoseString = "You lost!";
+		}
+		canvasController = ctrl;
+		CSVReaderWriter csvRW = new CSVReaderWriter();
+		csvRW.updateResult(winLose, currentUsername);
+	}
 
-      winLoseString = "You won with " + timeLeft + "Seconds left!";
-    } else {
-      winLoseLabel.setText("You lost!");
-      winLoseLabel2.setText("");
-      winLoseString = "You lost!";
-    }
-    canvasController = ctrl;
-    CSVReaderWriter csvRW = new CSVReaderWriter();
-    csvRW.updateResult(winLose, currentUsername);
-  }
+	public void timeLeft(int sec) throws IOException, CsvException {
+		timeLeft = sec;
+		CSVReaderWriter csvRW = new CSVReaderWriter();
+		csvRW.updateTime(60 - timeLeft, currentUsername);
 
-  public void timeLeft(int sec) throws IOException, CsvException {
-    timeLeft = sec;
-    CSVReaderWriter csvRW = new CSVReaderWriter();
-    csvRW.updateTime(60 - timeLeft, currentUsername);
+	}
 
-  }
-  public void getUsername(String username){
-    // Check if username is not null
-    if (username != null) {
-      // If not null, update label as current username
-      currentUsername = username;
+	public void getUsername(String username) {
+		// Check if username is not null
+		if (username != null) {
+			// If not null, update label as current username
+			currentUsername = username;
 
-    }
-  }
+		}
+	}
 
-  @FXML
-  private void onSave() {
-    Stage stage = (Stage) playAgainButton.getScene().getWindow(); // gets the stage from the button
-    FileChooser fileChooser = new FileChooser();
-    fileChooser.setTitle("Save Image");
-    File file =
-        fileChooser.showSaveDialog(
-            stage); // shows a popup to allow user to choose where to save file
-    if (file != null) {
-      try {
-        ImageIO.write(
-            canvasController.getCurrentSnapshot(),
-            "bmp",
-            file); // creates a new image to save to the users location
-      } catch (IOException ex) {
-        System.out.println(ex.getMessage());
-      }
-    }
-  }
+	@FXML
+	private void onSave() {
+		Stage stage = (Stage) playAgainButton.getScene().getWindow(); // gets the stage from the button
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Save Image");
+		File file = fileChooser.showSaveDialog(stage); // shows a popup to allow user to choose where to save file
+		if (file != null) {
+			try {
+				ImageIO.write(canvasController.getCurrentSnapshot(), "bmp", file); // creates a new image to save to the
+																					// users location
+			} catch (IOException ex) {
+				System.out.println(ex.getMessage());
+			}
+		}
+	}
 
+	@FXML
+	private void onMenu() throws IOException {
+		Stage stage = (Stage) menuButton.getScene().getWindow();
+		FXMLLoader loader = new FXMLLoader(App.class.getResource("/fxml/main_menu.fxml")); // reset to a new word_page
+																							// where a new word will be
+		// generated
+		Scene scene = new Scene(loader.load(), 1000, 680);
+		stage.setScene(scene);
+		stage.show();
 
-  @FXML
-  private void onPlayAgain() throws IOException, URISyntaxException, CsvException {
-    Stage stage = (Stage) playAgainButton.getScene().getWindow();
-    FXMLLoader loader =
-        new FXMLLoader(
-            App.class.getResource(
-                "/fxml/main_menu.fxml")); // reset to a new word_page where a new word will be
-    // generated
-    Scene scene = new Scene(loader.load(), 1000, 680);
-    stage.setScene(scene);
-    stage.show();
+		MainMenuController ctrl = loader.getController(); // gets controller of new page to pass text to speech
+		ctrl.give(textToSpeechBackground, textToSpeech); // passes text to speech instance and boolean to next page
+		ctrl.getUsername(currentUsername);
+	}
 
-    MainMenuController ctrl =
-        loader.getController(); // gets controller of new page to pass text to speech
-    ctrl.give(
-        textToSpeechBackground,
-        textToSpeech); // passes text to speech instance and boolean to next page
-    ctrl.getUsername(currentUsername);
-  }
+	@FXML
+	private void onPlayAgain() throws IOException, URISyntaxException, CsvException {
+		Stage stage = (Stage) playAgainButton.getScene().getWindow();
+		FXMLLoader loader = new FXMLLoader(App.class.getResource("/fxml/word_page.fxml")); // reset to a new word_page
+																							// where a new word will be
+		// generated
+		Scene scene = new Scene(loader.load(), 1000, 680);
+		stage.setScene(scene);
+		stage.show();
 
-  @FXML
-  private void onHoverTitle() {
-    textToSpeechBackground.backgroundSpeak("Game over", textToSpeech);
-  }
+		WordPageController ctrl = loader.getController(); // gets controller of new page to pass text to speech
+		ctrl.give(textToSpeechBackground, textToSpeech); // passes text to speech instance and boolean to next page
+		ctrl.getUsername(currentUsername);
+	}
 
-  @FXML
-  private void onHoverWinLose() {
-    textToSpeechBackground.backgroundSpeak("You " + winLoseString, textToSpeech);
-  }
+	@FXML
+	private void onHoverTitle() {
+		textToSpeechBackground.backgroundSpeak("Game over", textToSpeech);
+	}
 
-  @FXML
-  private void onHoverSave() {
-    textToSpeechBackground.backgroundSpeak("Save Button", textToSpeech);
-  }
+	@FXML
+	private void onHoverWinLose() {
+		textToSpeechBackground.backgroundSpeak("You " + winLoseString, textToSpeech);
+	}
 
-  @FXML
-  private void onHoverPlayAgain() {
-    textToSpeechBackground.backgroundSpeak("Play Again Button", textToSpeech);
-  }
+	@FXML
+	private void onHoverSave() {
+		textToSpeechBackground.backgroundSpeak("Save Button", textToSpeech);
+	}
 
-  @FXML
-  private void onHoverTextToSpeechLabel() {
-    textToSpeechBackground.backgroundSpeak("ON", textToSpeech);
-  }
+	@FXML
+	private void onHoverPlayAgain() {
+		textToSpeechBackground.backgroundSpeak("Play Again Button", textToSpeech);
+	}
 
-  @FXML
-  private void onTextToSpeech() {
-    textToSpeech = !textToSpeech; // inverts boolean
-    if (textToSpeech) { // then sets label accordingly
-      textToSpeechLabel.setText("ON");
-    } else {
-      textToSpeechLabel.setText("OFF");
-    }
-  }
+	@FXML
+	private void onHoverTextToSpeechLabel() {
+		textToSpeechBackground.backgroundSpeak("ON", textToSpeech);
+	}
 
-  @FXML
-  private void onHoverTextToSpeech() {
-    textToSpeechBackground.backgroundSpeak("toggle text to speech", textToSpeech);
-  }
+	@FXML
+	private void onTextToSpeech() {
+		textToSpeech = !textToSpeech; // inverts boolean
+		if (textToSpeech) { // then sets label accordingly
+			textToSpeechLabel.setText("ON");
+		} else {
+			textToSpeechLabel.setText("OFF");
+		}
+	}
 
-  @FXML
-  private void onHoverLogo() {
-    textToSpeechBackground.backgroundSpeak("Speedy Sketchers logo", textToSpeech);
-  }
+	@FXML
+	private void onHoverTextToSpeech() {
+		textToSpeechBackground.backgroundSpeak("toggle text to speech", textToSpeech);
+	}
+
+	@FXML
+	private void onHoverLogo() {
+		textToSpeechBackground.backgroundSpeak("Speedy Sketchers logo", textToSpeech);
+	}
 
 }
