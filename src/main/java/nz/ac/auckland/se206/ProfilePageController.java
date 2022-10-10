5c61742bb7d58ef56fa1c4bfbc4a8766e9ce0a11
tpo920 <tpo920@aucklanduni.ac.nz>
2022-10-10 17:50:55 +1300

    feature: Added tab system for profile page and badge progress bar

--- src/main/java/nz/ac/auckland/se206/ProfilePageController.java
+++ src/main/java/nz/ac/auckland/se206/ProfilePageController.java
@@ -1,210 +1,346 @@
 package nz.ac.auckland.se206;
 
-import com.opencsv.exceptions.CsvException;
 import java.io.IOException;
 import java.text.DecimalFormat;
+
+import com.opencsv.exceptions.CsvException;
+
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import nz.ac.auckland.se206.speech.TextToSpeechBackground;
 
 public class ProfilePageController {
 
-  @FXML private ImageView userImage;
-  @FXML private Button badgeButton;
-  @FXML private Button backButton;
-  @FXML private Label usernameLabel;
-  @FXML private Label winLabel;
-  @FXML private Label gameLabel;
-  @FXML private Label winrateLabel;
-  @FXML private Label fastestLabel;
-  @FXML private Label textToSpeechLabel;
-  @FXML private ImageView volumeImage;
-  @FXML private ListView<String> historyListView;
+	@FXML
+	private ImageView userImage;
+	@FXML
+	private Button badgeButton;
+	@FXML
+	private Button backButton;
+	@FXML
+	private Label usernameLabel;
+	@FXML
+	private Label winLabel;
+	@FXML
+	private Label gameLabel;
+	@FXML
+	private Label winrateLabel;
+	@FXML
+	private Label fastestLabel;
+	@FXML
+	private Label textToSpeechLabel;
+	@FXML
+	private Label badgeLabel, badgePercentage;
+	@FXML
+	private Label historyLabel;
+	@FXML
+	private ImageView volumeImage;
+	@FXML
+	private ListView<String> historyListView;
+	// Badges
+	@FXML
+	public Label numBadgeLabel;
+	@FXML
+	private ImageView secTen, secThirty;
+	@FXML
+	private ImageView fiveGames, tenGames, fiftyGames, hundredGames;
+	@FXML
+	public ImageView fiveStreak, tenStreak, fiftyStreak, hundredStreak;
+	@FXML
+	public ImageView easyWins, mediumWins, hardWins, masterWins;
+	@FXML
+	private ImageView godArtist;
 
-  private Boolean textToSpeech;
-  private TextToSpeechBackground textToSpeechBackground;
-  private String currentUsername;
-  private String currentWord;
-  private int usersLosses;
-  private int usersWins;
-  private int totalGames;
-  private int fastestTime;
-  private double winRate;
-  private DecimalFormat df = new DecimalFormat("#.#");
-  private String[] historyWords;
+	@FXML
+	private ProgressBar badgeProgress;
 
-  public void initialize() {}
+	private Boolean textToSpeech;
+	private TextToSpeechBackground textToSpeechBackground;
+	private String currentUsername;
+	private String currentWord;
+	private int usersLosses;
+	private int usersWins;
+	private int totalGames;
+	private int fastestTime;
+	private double badgeRatio;
+	private double winRate;
+	private DecimalFormat df = new DecimalFormat("#.#");
+	private int numberBadges = 0;
+	private String[] historyWords;
 
-  public void onHoverTextToSpeechLabel() {
-    textToSpeechBackground.backgroundSpeak("toggle text to speech", textToSpeech);
-  }
+	public void initialize() {
+	}
 
-  public void onHoverTextToSpeech() {
-    textToSpeechBackground.backgroundSpeak("On", textToSpeech);
-    volumeImage.setFitHeight(48);
-    volumeImage.setFitWidth(48);
-  }
+	public void onHoverTextToSpeechLabel() {
+		textToSpeechBackground.backgroundSpeak("toggle text to speech", textToSpeech);
+	}
 
-  public void give(TextToSpeechBackground textToSpeechBackground, Boolean textToSpeech) {
-    this.textToSpeech = textToSpeech;
-    this.textToSpeechBackground = (textToSpeechBackground);
-    if (textToSpeech) { // updates text to speech label to ensure it is up-to-date
-      textToSpeechLabel.setText("ON");
-    }
-  }
+	public void onHoverTextToSpeech() {
+		textToSpeechBackground.backgroundSpeak("On", textToSpeech);
+		volumeImage.setFitHeight(48);
+		volumeImage.setFitWidth(48);
+	}
 
-  public void setUsername(String username) throws IOException, CsvException {
-    // Check if username is not null
-    if (username != null) {
-      // If not null, update label as current username
-      currentUsername = username;
-      this.usernameLabel.setText(currentUsername);
-      SpreadSheetReaderWriter spreadSheetReaderWriter = new SpreadSheetReaderWriter();
+	@FXML
+	private void onHoverNumBadges() {
+		textToSpeechBackground.backgroundSpeak("You have " + numberBadges + "badges", textToSpeech);
+	}
 
-      // Assign wins
-      usersWins = spreadSheetReaderWriter.getWins(currentUsername);
-      usersLosses = spreadSheetReaderWriter.getLosses(currentUsername);
-      fastestTime = spreadSheetReaderWriter.getFastest(currentUsername);
-      historyWords = spreadSheetReaderWriter.getHistory(currentUsername).split(",");
-      currentWord = spreadSheetReaderWriter.getHistory(currentUsername);
+	public void give(TextToSpeechBackground textToSpeechBackground, Boolean textToSpeech) {
+		this.textToSpeech = textToSpeech;
+		this.textToSpeechBackground = (textToSpeechBackground);
+		if (textToSpeech) { // updates text to speech label to ensure it is up-to-date
+			textToSpeechLabel.setText("ON");
+		}
+	}
 
-      // Calculate games
-      totalGames = usersWins + usersLosses;
+	// Set username, stats, and badges
+	public void setUsername(String username) throws IOException, CsvException {
+		// Check if username is not null
+		if (username != null) {
+			// If not null, update label as current username
+			currentUsername = username;
+			this.usernameLabel.setText(currentUsername);
+			SpreadSheetReaderWriter spreadSheetReaderWriter = new SpreadSheetReaderWriter();
 
-      if (totalGames > 0) {
-        winRate = (((double) usersWins * 100) / (double) totalGames);
-      }
+			// Assign wins
+			usersWins = spreadSheetReaderWriter.getWins(currentUsername);
+			usersLosses = spreadSheetReaderWriter.getLosses(currentUsername);
+			fastestTime = spreadSheetReaderWriter.getFastest(currentUsername);
+			historyWords = spreadSheetReaderWriter.getHistory(currentUsername).split(",");
+			currentWord = spreadSheetReaderWriter.getHistory(currentUsername);
 
-      // Update Labels
-      winLabel.setText(Integer.toString(usersWins));
-      gameLabel.setText(Integer.toString(totalGames));
-      winrateLabel.setText(df.format(winRate) + "%");
+			// Calculate games
+			totalGames = usersWins + usersLosses;
 
-      if (fastestTime == 100) { // value will be 100 by default eg they must play a game
-        fastestLabel.setText("-");
-      } else {
-        fastestLabel.setText(fastestTime + " seconds");
-      }
+			if (totalGames > 0) {
+				winRate = (((double) usersWins * 100) / (double) totalGames);
+			}
 
-      // Add current word to history of words
-      if (!currentWord.equals("none")) {
-        historyListView.getItems().addAll(historyWords);
-      }
+			// Update Labels
+			winLabel.setText(Integer.toString(usersWins));
+			gameLabel.setText(Integer.toString(totalGames));
+			winrateLabel.setText(df.format(winRate) + "%");
 
-    } else {
-      // If user is not signed in
-      this.usernameLabel.setText("Guest");
-      winLabel.setText("-");
-      fastestLabel.setText("-");
-      gameLabel.setText("-");
-      winrateLabel.setText("-");
-    }
-  }
+			if (fastestTime == 100) { // value will be 100 by default eg they must play a game
+				fastestLabel.setText("-");
+			} else {
+				fastestLabel.setText(fastestTime + " seconds");
+			}
 
-  @FXML
-  private void onBack() throws IOException {
-    Stage stage = (Stage) backButton.getScene().getWindow();
-    LoadPage loadPage = new LoadPage();
-    loadPage.extractedMainMenu(textToSpeechBackground, textToSpeech, currentUsername, stage);
-  }
+			// Add current word to history of words
+			if (!currentWord.equals("none")) {
+				historyListView.getItems().addAll(historyWords);
+			}
 
-  @FXML
-  private void onTextToSpeech() {
-    textToSpeech = !textToSpeech; // inverts boolean of text to speech
-    if (textToSpeech) { // sets label accordingly
-      textToSpeechLabel.setText("ON");
-    } else {
-      textToSpeechLabel.setText("OFF");
-    }
-  }
+			// Set badges
+			setBadges();
+			numBadgeLabel.setText(String.valueOf(numberBadges));
+			badgeLabel.setText("You've unlocked " + numberBadges + "/15" + " Badges");
 
-  // Below is list of methods for when mouse hovers a button
-  @FXML
-  private void onHoverBack() {
-    textToSpeechBackground.backgroundSpeak("Back Button", textToSpeech);
-    backButton.setStyle(
-        "-fx-background-radius: 100px; -fx-text-fill: white; -fx-border-radius: 100px; -fx-background-color: #99DAF4; -fx-border-color: #99DAF4;");
-  }
+			// Calculate percentage of badges completed
+			badgeRatio = (((double) numberBadges * 100) / (double) 15);
+			badgePercentage.setText("(" + String.format("%.0f", badgeRatio) + "%)");
+			badgeProgress.setProgress(badgeRatio / 100);
+		} else {
+			// If user is not signed in
+			this.usernameLabel.setText("Guest");
+			winLabel.setText("-");
+			fastestLabel.setText("-");
+			gameLabel.setText("-");
+			winrateLabel.setText("-");
 
-  // Below is list of methods for when mouse exits a button
-  @FXML
-  private void onBackExit() {
-    backButton.setStyle(
-        "-fx-background-radius: 100px; -fx-text-fill: white; -fx-background-color: #EB4A5A; -fx-text-fill: white; -fx-border-color: white; -fx-border-radius: 100px;");
-  }
+			// Set badges
+			badgeLabel.setText("You've unlocked 0/15 Badges");
+			badgePercentage.setText("(0%)");
+			numBadgeLabel.setText("-");
+			setAllBadgesClear();
+		}
+	}
 
-  @FXML
-  private void onVolumeExit() {
-    volumeImage.setFitHeight(45);
-    volumeImage.setFitWidth(45);
-  }
+	@FXML
+	private void onBack() throws IOException {
+		Stage stage = (Stage) backButton.getScene().getWindow();
+		LoadPage loadPage = new LoadPage();
+		loadPage.extractedMainMenu(textToSpeechBackground, textToSpeech, currentUsername, stage);
+	}
 
-  @FXML
-  private void onHoverUser() {
-    textToSpeechBackground.backgroundSpeak(currentUsername, textToSpeech);
-  }
+	@FXML
+	private void onTextToSpeech() {
+		textToSpeech = !textToSpeech; // inverts boolean of text to speech
+		if (textToSpeech) { // sets label accordingly
+			textToSpeechLabel.setText("ON");
+		} else {
+			textToSpeechLabel.setText("OFF");
+		}
+	}
 
-  @FXML
-  private void onHoverWinRate() {
-    textToSpeechBackground.backgroundSpeak(winRate + "win rate", textToSpeech);
-  }
+	// Method to display selected word in list of words
+	@FXML
+	private void onSelectWord(MouseEvent event) {
+		String word = historyListView.getSelectionModel().getSelectedItem();
 
-  @FXML
-  private void onHoverHistory() {
-    textToSpeechBackground.backgroundSpeak("History of words played", textToSpeech);
-  }
+		if (word == null || word.isEmpty()) {
+			historyLabel.setText("Nothing Selected");
+		} else {
+			historyLabel.setText(word);
+		}
 
-  @FXML
-  private void onHoverTitle() {
-    textToSpeechBackground.backgroundSpeak("Just Draw", textToSpeech);
-  }
+	}
 
-  @FXML
-  private void onHoverGamesPlayed() {
-    textToSpeechBackground.backgroundSpeak("Played" + totalGames + "Games", textToSpeech);
-  }
+	// Badge Methods
+	private void setAllBadgesClear() {
+		// streak badges set to transparent
+		fiveStreak.setOpacity(0.2);
+		tenStreak.setOpacity(0.2);
+		fiftyStreak.setOpacity(0.2);
+		hundredStreak.setOpacity(0.2);
 
-  @FXML
-  private void onHoverGamesWon() {
-    textToSpeechBackground.backgroundSpeak("you have won" + usersWins, textToSpeech);
-  }
+		// Games played to be added
 
-  @FXML
-  private void onHoverFastest() {
-    textToSpeechBackground.backgroundSpeak(
-        "fastest game was " + fastestTime + "seconds", textToSpeech);
-  }
+		// difficulty badges set to transparent
+		easyWins.setOpacity(0.2);
+		mediumWins.setOpacity(0.2);
+		hardWins.setOpacity(0.2);
+		masterWins.setOpacity(0.2);
 
-  public void onClickBadge() throws IOException, CsvException {
-    Stage stage = (Stage) backButton.getScene().getWindow();
-    FXMLLoader loader =
-        new FXMLLoader(
-            App.class.getResource("/fxml/badge_page.fxml")); // creates a new instance of page
-    Scene scene = new Scene(loader.load(), 1000, 680);
-    BadgePageController ctrl = loader.getController(); // need controller to pass information
-    // may need to add code to pass though tts here
-    ctrl.give(textToSpeechBackground, textToSpeech); // passes text to speech instance and boolean
-    ctrl.setUsername(currentUsername);
-    stage.setScene(scene);
-    stage.show();
-  }
+		// fastest game badges set to transparent
+		secThirty.setOpacity(0.2);
+		secTen.setOpacity(0.2);
 
-  @FXML
-  private void onHoverBadge() {
-    textToSpeechBackground.backgroundSpeak("Badge", textToSpeech);
-    badgeButton.setStyle(
-        "-fx-background-radius: 100px; -fx-text-fill: white; -fx-border-radius: 100px; -fx-background-color: #99DAF4; -fx-border-color: #99DAF4;");
-  }
+		// Special badges set to transparent
+		godArtist.setOpacity(0.2);
+	}
 
-  @FXML
-  private void onBadgeExit() {
-    badgeButton.setStyle(
-        "-fx-background-radius: 100px; -fx-text-fill: white; -fx-background-color: #EB4A5A; -fx-text-fill: white; -fx-border-color: white; -fx-border-radius: 100px;");
-  }
+	private void setBadges() throws IOException, CsvException {
+		setTimeBadges();
+		// setGamesPlayedBadges();
+		setWinStreakBadges();
+		setDifficultWinBadges();
+		setExtraBadges();
+	}
+
+	private void setExtraBadges() {
+		godArtist.setOpacity(0.2);
+		// TODO: once difficulties are added
+	}
+
+	private void setDifficultWinBadges() {
+		easyWins.setOpacity(0.2);
+		mediumWins.setOpacity(0.2);
+		hardWins.setOpacity(0.2);
+		masterWins.setOpacity(0.2);
+		// TODO: once difficulties are added
+	}
+
+	private void setWinStreakBadges() throws IOException, CsvException {
+		SpreadSheetReaderWriter sheetReaderWriter = new SpreadSheetReaderWriter();
+		int streak = sheetReaderWriter.getStreak(currentUsername);
+		// set all transparent
+		fiveStreak.setOpacity(0.2);
+		tenStreak.setOpacity(0.2);
+		fiftyStreak.setOpacity(0.2);
+		hundredStreak.setOpacity(0.2);
+		if (streak >= 5) { // 5 win streak
+			fiveStreak.setOpacity(1);
+			numberBadges++;
+		}
+		if (streak >= 10) { // 10 win streak
+			tenStreak.setOpacity(1);
+			numberBadges++;
+		}
+		if (streak >= 50) { // 50 win streak
+			fiftyStreak.setOpacity(1);
+			numberBadges++;
+		}
+		if (streak >= 100) {
+			hundredStreak.setOpacity(1);
+			numberBadges++;
+		}
+	}
+
+	private void setTimeBadges() throws IOException, CsvException {
+		SpreadSheetReaderWriter sheetReaderWriter = new SpreadSheetReaderWriter();
+		int fastest = sheetReaderWriter.getFastest(currentUsername);
+		// set all transparent
+		secThirty.setOpacity(0.2);
+		secTen.setOpacity(0.2);
+		if (fastest <= 30) {
+			secThirty.setOpacity(1);
+			numberBadges++;
+			// 30 second badge
+		}
+		if (fastest <= 10) {
+			secTen.setOpacity(1);
+			numberBadges++;
+			// 10 second badge
+		}
+	}
+
+	// Below is list of methods for when mouse hovers a button
+	@FXML
+	private void onHoverBack() {
+		textToSpeechBackground.backgroundSpeak("Back Button", textToSpeech);
+		backButton.setStyle(
+				"-fx-background-radius: 100px; -fx-text-fill: white; -fx-border-radius: 100px; -fx-background-color: #99DAF4; -fx-border-color: #99DAF4;");
+	}
+
+	// Below is list of methods for when mouse exits a button
+	@FXML
+	private void onBackExit() {
+		backButton.setStyle(
+				"-fx-background-radius: 100px; -fx-background-color: #EB4A5A; -fx-text-fill: white; -fx-border-color: white; -fx-border-radius: 100px;");
+	}
+
+	@FXML
+	private void onVolumeExit() {
+		volumeImage.setFitHeight(45);
+		volumeImage.setFitWidth(45);
+	}
+
+	@FXML
+	private void onHoverUser() {
+		textToSpeechBackground.backgroundSpeak(currentUsername, textToSpeech);
+	}
+
+	@FXML
+	private void onHoverWinRate() {
+		textToSpeechBackground.backgroundSpeak(winRate + "win rate", textToSpeech);
+	}
+
+	@FXML
+	private void onHoverHistory() {
+		textToSpeechBackground.backgroundSpeak("History of words played", textToSpeech);
+	}
+
+	@FXML
+	private void onHoverTitle() {
+		textToSpeechBackground.backgroundSpeak("Just Draw", textToSpeech);
+	}
+
+	@FXML
+	private void onHoverGamesPlayed() {
+		textToSpeechBackground.backgroundSpeak("Played" + totalGames + "Games", textToSpeech);
+	}
+
+	@FXML
+	private void onHoverGamesWon() {
+		textToSpeechBackground.backgroundSpeak("you have won" + usersWins, textToSpeech);
+	}
+
+	@FXML
+	private void onHoverFastest() {
+		textToSpeechBackground.backgroundSpeak("fastest game was " + fastestTime + "seconds", textToSpeech);
+	}
+
+	// Progress bar methods
+	public void setBadgeProgress() {
+
+	}
 }
