ad12e43ebebd17c02fcc4ecd177c2e06c13ec773
tpo920 <tpo920@aucklanduni.ac.nz>
2022-10-16 15:46:39 +1300

    fix: Updated layout of canvas and hidden word mode

--- src/main/java/nz/ac/auckland/se206/words/WordPageController.java
+++ src/main/java/nz/ac/auckland/se206/words/WordPageController.java
@@ -230,7 +230,7 @@ public class WordPageController {
       // text to speech
       // and whether it is on or not
       hiddenWordCanvasController.setDefinitionList(currentWord);
-      hiddenWordCanvasController.getUsername(currentUsername);
+      hiddenWordCanvasController.getUsername(currentUsername, currentProfilePic);
     }
     stage.show();
   }
